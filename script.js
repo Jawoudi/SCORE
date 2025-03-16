3e31b65c9ec4480b5ba9e8f97aeb7b28fce4a355
Jawoudi <nunoxsmurfcat@gmail.com>
2025-03-16 10:35:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
         { id: 1, team1: "Paris Saint-Germain", team2: "Olympique de Marseille", score1: 0, score2: 0, date: "2025-03-16", time: "20:45", startTime: null, finished: false, status: "upcoming" },
         { id: 2, team1: "Atlético Madrid", team2: "FC Barcelone", score1: 0, score2: 0, date: "2025-03-16", time: "21:00", startTime: null, finished: false, status: "upcoming" },
         { id: 3, team1: "Sevilla FC", team2: "Athletic Bilbao", score1: 0, score2: 0, date: "2025-03-16", time: "16:15", startTime: null, finished: false, status: "upcoming" },
-        { id: 4, team1: "Leicester City", team2: "Manchester United", score1: 0, score2: 0, date: "2025-03-16", time: "10:00", startTime: null, finished: false, status: "upcoming" },
+        { id: 4, team1: "Leicester City", team2: "Manchester United", score1: 1, score2: 0, date: "2025-03-16", time: "10:00", startTime: null, finished: false, status: "upcoming" },
         { id: 5, team1: "Montpellier", team2: "Saint-Étienne", score1: 0, score2: 0, date: "2025-03-16", time: "13:30", startTime: null, finished: false, status: "upcoming" },
         { id: 6, team1: "Fulham", team2: "Tottenham Hotspur", score1: 0, score2: 0, date: "2025-03-16", time: "14:30", startTime: null, finished: false, status: "upcoming" },
         { id: 7, team1: "Arsenal", team2: "Chelsea", score1: 0, score2: 0, date: "2025-03-16", time: "14:30", startTime: null, finished: false, status: "upcoming" },
